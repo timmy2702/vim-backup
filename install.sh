e49b32cd99aada39dff4c801bd9232c27e2d44df
Tim Nguyen <timmy2702@users.noreply.github.com>
2018-03-10 12:24:57 -0800

    Fix command line bug

--- install.sh
+++ install.sh
@@ -38,7 +38,8 @@ git clone https://github.com/scrooloose/nerdtree.git
 # Install syntastic
 git clone --depth=1 https://github.com/scrooloose/syntastic.git
 
-# Install vim-airline git clone https://github.com/vim-airline/vim-airline.git
+# Install vim-airline 
+git clone https://github.com/vim-airline/vim-airline.git
 
 # Install vim-cpp-enhanced-highlight
 git clone https://github.com/octol/vim-cpp-enhanced-highlight.git
